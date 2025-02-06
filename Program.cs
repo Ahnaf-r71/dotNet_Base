1265104afe72d1bbe6955fe0be4db6aab534f476
Ahnaf-r71 <ahnafr71@gmail.com>
2025-02-06 17:08:45 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates; // better practice when it comes to accessing members of different class 
+// by default system class is inaugarated
+class Test{
+    public static void Main(string[] args){
+        Console.Write("this prints line without newline");
+        string fullName="Ahnaf Rashid";
+        System.Console.WriteLine("\nTest");
+        Console.WriteLine("\"Hello world!\""+fullName); 
+        const string familyName="Rashid";
+        Console.WriteLine("My family name is "+familyName);
+        
+        // type casting
+        double salary = 11.99;
+        int intSalary = (int)salary;//to cast
+        Console.WriteLine("Salary as integer: "+intSalary);
+        string storeString= Convert.ToString(intSalary);
+        Console.WriteLine("Salary as string: "+storeString);
+        string salary2="11"; 
+        int store=int.Parse(salary2); //parse to convert from a string
+        Console.Write(store);
+
+        bool isSuccess=int.TryParse(salary2,out int result);
+        Console.Write("\n"+result,isSuccess);
+        Console.WriteLine($"True or False is={isSuccess}");
+    }
+}
